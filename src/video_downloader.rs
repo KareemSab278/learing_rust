3610093f94d57e4f828dee4bd8a01fe4d263d4b4
KareemSab278 <kareemsab278@gmail.com>
2026-01-10 21:14:15 +0000

    - made a video downloader lol

--- src/video_downloader.rs
+++ src/video_downloader.rs
@@ -11,17 +11,15 @@ use yt_dlp::client::deps::Libraries;
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let libraries_dir = PathBuf::from("libs");
     let output_dir = PathBuf::from("output");
-    std::fs::create_dir_all(&output_dir)?;
 
     let youtube = libraries_dir.join("yt-dlp");
     let ffmpeg = libraries_dir.join("ffmpeg");
 
     let libraries = Libraries::new(youtube, ffmpeg);
-    let fetcher = Youtube::new(libraries, output_dir).await?;
+    let fetcher = Youtube::new(libraries, output_dir)?;
 
     let url = String::from("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
     // need to find a way to get video title and set as the second param in download_audio_stream_from_url fn;
-    // fetcher.download_audio_stream_from_url(url, "audio.mp3").await?;
-    fetcher.download_audio_stream_from_url(url, "audio.m4a").await?;
+    fetcher.download_audio_stream_from_url(url, "audio.mp3").await?;
     Ok(())
 }
--- test.js
+++
@@ -1,1 +0,0 @@
-// https://github.com/boul2gom/yt-dlp?tab=readme-ov-file
