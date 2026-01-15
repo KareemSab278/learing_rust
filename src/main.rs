6f91d2c34e89107282d8237c35e936eee9eb42f2
KareemSab278 <kareemsab278@gmail.com>
2026-01-15 20:09:26 +0000

    couldnt figure out the other type of error handling today bruh

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,30 @@
-fn main() {
-    // more on structs
-    // let user1 = User::create_user(
-    //     1,
-    //     String::from("poop_master3000"),
-    //     String::from("poop"),
-    //     String::from("master"),
-    //     19690420 // YYYYMMDD
-    // );
-
-    // let black = HexaColor(0, 0, 0);
-
-    // println!("{:?}", user1); // debug mode only
-#[derive(Debug)]
-    enum IpAddKind { // you can use an enum as a type in a struct or as type in general.
-        V4(String),
-        V6(String), // you can declare them as string or anything else to define the data type
-    }
-
-    // YOU CAN USE STRUCTS:
-    struct IpAddr {
-        ip: IpAddKind,
-        name: String,
-    }
-
-    let ip_v4_add_kind = IpAddKind::V4;
-
-    let ip_address: IpAddr = {
-        IpAddr {
-            ip: ip_v4_add_kind,
-            name: String::from("IPV4"),
-        }
-    };
-
-    // OR SIMPLY JUST CALL IT DIRECTLY AFTER SETTING THE DATA TYPE:
-    
-    let I_P_V_4 = IpAddKind::V4(String::from("0.0.0.0"));
-    println!("{:?}", I_P_V_4);
-
-    // kinda like const ip = { kind: "V4", value: "0.0.0.0" }; in js
+enum Option<DataType> { // the type. if exists then cool
+    Some(DataType), // exists
+    None, // does not exist bruv
 }
 
-// #[derive(Debug)] // you need this to :? print a struct. only use in development
-// // without it youll get "cannot be formatted using `{:?}` because it doesn't implement `Debug`"
-// struct User {
-//     id: i64,
-//     user_name: String,
-//     first_name: String,
-//     last_name: String,
-//     dob: i32,
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E), // the type of err
 // }
 
-// impl User {
-//     fn create_user(
-//         id: i64,
-//         user_name: String,
-//         first_name: String,
-//         last_name: String,
-//         dob: i32
-//     ) -> User {
-//         User {
-//             id,
-//             user_name: user_name,
-//             first_name,
-//             last_name,
-//             dob,
-//         }
+// fn is_divisible_by_2(dividee: i32) -> Result<String, String> {
+//     if dividee % 2 == 0 {
+//         return Result::Ok("Divisible by 2".to_string());
 //     }
+//     return Result::Err("Not divisible by 2".to_string());
 // }
 
-// // tuple-like struct
-// struct HexaColor(i32, i32, i32);
+fn age_verification(age: i32) -> Option<i32> {
+    if age >= 18 {
+        return Option::Some(age);
+    }
+    return Option::None;
+}
+
+fn main() {
+    match age_verification(22) {
+        Option::Some(val) => println!("AGE OK: {}", val),
+        Option::None => println!("underage"),
+    }
+}
