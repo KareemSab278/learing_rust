31dc11a67eb9b82f394f30eb2fdfb84bcdb4401e
KareemSab278 <kareemsab278@gmail.com>
2026-02-02 19:44:07 +0000

    meh im tired

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@
 
 // Temperature Converter
 // Convert Celsius ↔ Fahrenheit from user input.
-enum TempType{
+enum TempType {
     Celsius,
-    Fahrenheit
+    Fahrenheit,
 }
 fn convert_temp(input: f32, tmp_type: TempType) -> f32 {
     // could have used char but char is always 4 bytes. str is 1 byte but can be bigger. for simple letter its 1b.
     // let input = input as f64; // i had no idea you coul do this....
-    match tmp_type{
-        TempType::Celsius => return (input * 1.8) + 32.0,
-        TempType::Fahrenheit => return (input - 32.0) / 1.8,
+    match tmp_type {
+        TempType::Celsius => {
+            return input * 1.8 + 32.0;
+        }
+        TempType::Fahrenheit => {
+            return (input - 32.0) / 1.8;
+        }
     }
 }
 // fn main() {
@@ -55,11 +59,51 @@ fn guess_loop(random_int: u32) {
 // Print numbers 1–100 with Fizz/Buzz rules.
 // Learn: conditionals, loops.
 
+fn fizz_buzz(mut i: u32) {
+    if i > 0 {
+        loop {
+            if i % 3 == 0 && i % 5 == 0 {
+                println!("{} - FizzBuzz", i);
+            } else if i % 5 == 0 {
+                println!("{} - Buzz", i);
+            } else if i % 3 == 0 {
+                println!("{} - Fizz", i);
+            } else {
+                println!("{} not fizz or buzz or fizzbuzz", i);
+            }
+            if i == 0 {
+                break;
+            }
+            i -= 1;
+        }
+    }
+}
+
+// fn main() {
+//     fizz_buzz(10);
+// }
+
 // 🟡 Level 2: Ownership & Collections
 
 // Word Counter
 // Read a sentence and count how many times each word appears.
 // Learn: HashMap, borrowing, lifetimes (light).
+use std::collections::HashMap;
+
+fn find_words(sentence: &str) {
+    let mut words_hmap = HashMap::new();
+    for word in sentence.split_whitespace() {
+        let count = words_hmap.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", words_hmap);
+}
+
+fn main() {
+    find_words(
+        "The HashMap should be declared inside the function or passed as a mutable reference."
+    )
+}
 
 // Reverse a String (Safely)
 // Reverse a UTF-8 string correctly (no .chars().rev().collect() shortcut at first 😉).
