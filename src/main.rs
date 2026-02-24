fa2f7ae5444269f2ec5b9f23bbf136448e59da85
KareemSab278 <kareemsab278@gmail.com>
2026-02-24 00:28:11 +0000

    did another one

--- src/main.rs
+++ src/main.rs
@@ -54,25 +54,63 @@
 //     rev_int(32);
 // }
 
-fn longest_substr(input: String) -> i16 {
-    let mut stack: Vec<char> = Vec::new();
-    let mut iter: usize = 0;
-    let input_c: Vec<char> = input.chars().collect();
-    while iter < input.len() {
-        if !stack.contains(&input_c[iter]) {
-            stack.push(input_c[iter]);
-        }
-        iter += 1;
+// fn longest_substr(input: String) -> i16 {
+//     let mut stack: Vec<char> = Vec::new();
+//     let mut iter: usize = 0;
+//     let input_c: Vec<char> = input.chars().collect();
+//     while iter < input.len() {
+//         if !stack.contains(&input_c[iter]) {
+//             stack.push(input_c[iter]);
+//         }
+//         iter += 1;
+//     }
+//     let stacklen= stack.len();
+//     let output: i16 = stacklen as i16;
+//     println!("{}", output);
+//     return output;
+
+//     // i just realized i coul have used a set... but idk how in rust yet lol
+// }
+
+// fn main() {
+//     let test_str: String = "veveveveveveve".to_string();
+//     longest_substr(test_str);
+// }
+
+fn find_medean(input1: Vec<i32>, input2: Vec<i32>)-> Result<f32, bool>{
+    // rust has a spread operator: y.iter().chain(&input);
+    // OR: y.iter().chain(&x).map(|&x|x).collect();
+    // chain is an iterator that combines two iterators together like a chain. <- !!!
+    if input1.len() == 0 || input2.len() == 0 {
+        return Err(false);
     }
-    let stacklen= stack.len();
-    let output: i16 = stacklen as i16;
-    println!("{}", output);
-    return output;
+    let combined_input: Vec<i32> = input1.iter().chain(&input2).map(|&x|x).collect(); // this works apparently so thats cool.
+    let input_len = combined_input.len() as f32;
+    let mut output: f32 = 0.0;
+    
+    for num in combined_input {
+        output += num as f32;
+    }
 
-    // i just realized i coul have used a set... but idk how in rust yet lol
+    output = output/input_len;
+
+    // Add up all of the numbers and divide by the number of numbers in the data set.
+
+    println!("output: {}", output);
+    Ok(output)
 }
 
-fn main() {
-    let test_str: String = "veveveveveveve".to_string();
-    longest_substr(test_str);
+fn main(){
+    let input1: Vec<i32> = [1,2,3,4,5,].to_vec();
+    let input2: Vec<i32> = [6,7,8, 9, 10, 11, 12, 13, 90].to_vec();
+    let medean = find_medean(input1, input2);
+    if medean == Err(false){
+        println!("one of your inputs was empty bruh");
+    }
 }
+
+
+
+// ====================================================================================================================================
+// NOTES: 
+// combine vecs: let combined_input: Vec<i32> = input1.iter().chain(&input2).map(|&x|x).collect();
