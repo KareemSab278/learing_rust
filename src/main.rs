60759c4e0d48deb7f854f23154df7d00fe5ea70d
KareemSab278 <KareemSab278@gmail.com>
2026-07-02 19:54:21 +0100

    encode and decode (with implementation)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod challenges;
 mod neet_code;
 
 fn main() {
-    let nums : Vec<i32> = Vec::from([1,2,1,1,1,3,4,4,4]);
-    println!("{:?}", neet_code::top_k_frequent(nums, 2));
+    
+    println!("{:?}", neet_code::Solution::encode(vec!["Hello".to_string(), "World".to_string()]));
+    println!("{:?}", neet_code::Solution::decode("Hello@^^^@World".to_string()));
 }
--- src/neet_code.rs
+++ src/neet_code.rs
@@ -1,37 +1,40 @@
 // gonna try doing 1 neetcode problem a day.
-use std::collections::HashMap;
+use std::collections::{ HashMap, HashSet };
 
-pub fn has_duplicate(nums: Vec<i32>) -> bool { // return true if there are duplicates in the array, false otherwise
+#[allow(dead_code)]
+pub fn has_duplicate(nums: Vec<i32>) -> bool {
+    // return true if there are duplicates in the array, false otherwise
     // do a set from the array and compare the length of the set to the length of the array
-    let set: std::collections::HashSet<i32> = nums.clone().into_iter().collect();
+    let set: HashSet<i32> = nums.clone().into_iter().collect();
     set.len() != nums.len()
 }
 
+#[allow(dead_code)]
 pub fn valid_anagram(phrase: &str, phrase1: &str) -> bool {
-	// has same letters
-	let len_check = phrase.len() == phrase1.len();
-		if len_check == false {
-			return false
+    // has same letters
+    let len_check = phrase.len() == phrase1.len();
+    if len_check == false {
+        return false;
+    }
+
+    // build a hashmap from the words now
+    let mut hashmap = HashMap::new();
+    let mut hashmap1 = HashMap::new();
+
+    for c in phrase.chars() {
+        *hashmap.entry(c).or_insert(0) += 1;
+    }
+
+    for c in phrase1.chars() {
+        *hashmap1.entry(c).or_insert(0) += 1;
+    }
+
+    hashmap == hashmap1
 }
-
-// build a hashmap from the words now
-	let mut hashmap = std::collections::HashMap::new();
-	let mut hashmap1 = std::collections::HashMap::new();
-
-	for c in phrase.clone().chars() {
-		*hashmap.entry(c).or_insert(0) += 1;
-	}
-
-	for c in phrase1.clone().chars() {
-		*hashmap1.entry(c).or_insert(0) += 1;
-	}
-
-	hashmap == hashmap1
-}
-
 
 //return the index of the numbers that add up to target
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32>{
+#[allow(dead_code)]
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     // sort the vec first, then find out where the two numbers add up
     // then find the idnexes for those nums in the original vec
 
@@ -39,29 +42,26 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32>{
     let mut second = nums.len() as i32;
     let mut output: Vec<i32> = Vec::new();
     second -= 1;
-    
+
     while first < second {
-        if nums[first as usize] + nums[second as usize] > target{
-            second -= 1
+        if nums[first as usize] + nums[second as usize] > target {
+            second -= 1;
         }
-        if nums[first as usize] + nums[second as usize] < target{
+        if nums[first as usize] + nums[second as usize] < target {
             first += 1;
-        }
-        else {
-
-    output.push(first);
-    output.push(second);
-    return output;
-
+        } else {
+            output.push(first as i32);
+            output.push(second as i32);
+            return output;
         }
     }
 
     output
 }
 
-
+#[allow(dead_code)]
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    let mut groups: std::collections::HashMap<String, Vec<String>> = std::collections::HashMap::new();
+    let mut groups: HashMap<String, Vec<String>> = HashMap::new();
 
     for s in strs {
         // create sorted version of the word
@@ -76,7 +76,7 @@ pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     groups.into_values().collect()
 }
 
-
+#[allow(dead_code)]
 pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let mut counts = HashMap::new();
 
@@ -92,4 +92,16 @@ pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
         .take(k as usize)
         .map(|(num, _)| num)
         .collect()
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn encode(s: Vec<String>) -> String {
+        s.join("@^^^@")
+    }
+
+    pub fn decode(s: String) -> Vec<String> {
+        s.split("@^^^@").map(|s| s.to_string()).collect()
+    }
 }
